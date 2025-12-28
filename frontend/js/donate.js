929c4f05d8a6a69c7d2b807d793ef7ab0dc54c7b
Arash Mardani <arashmardany@gmail.com>
2025-12-28 09:57:02 +0330

    Update donate.js

--- frontend/js/donate.js
+++ frontend/js/donate.js
@@ -9,7 +9,8 @@ const networks = {
                 name: "Polygon Amoy (تست‌نت)",
                 icon: "https://cryptologos.cc/logos/polygon-matic-logo.png",
                 addressField: "contractAddress",
-                usdtAddress: "0x41e94eb019c0762f9bfcf9fb78e59bec0a32e187",
+                usdtAddress: "0xf619f03999dfc383cFeaD476809eF3EEec420566",  // mock USDT تو
+                //usdtAddress: "0x41e94eb019c0762f9bfcf9fb78e59bec0a32e187",
                 chainId: 80002,
                 explorer: "https://amoy.polygonscan.com",
             },
@@ -305,6 +306,7 @@ function isTronReady() {
 
 // اجرای اولیه
 loadProject();
+
 
 
 
